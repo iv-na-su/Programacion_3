dae174b110275b098f51afc4f12a288cc4e07fdc
Ivan Nahuel Suarez <naahuel.suarez.2000@gmail.com>
2023-06-03 10:58:03 -0300

    Subida Agencia De Turismo

---
+++ Agencia De Turismo/Agencia De Turismo/CEjecutora.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agencia_De_Turismo
+{
+    class CEjecutora
+    {
+        static void Main()
+        {
+            ushort Numero;
+            CPaquete Menor = null, Paquete_act = null;
+
+            Console.Write("Ingresar porcentaje de impuesto de imposiciones brutas: %");
+            CPaquete.SetImpuesto(float.Parse(Console.ReadLine()));
+
+
+            Console.Write("Ingresar el codigo del paquete: ");
+            Numero = ushort.Parse(Console.ReadLine());
+
+
+            while(Numero != 0)
+            {
+                Console.Write("Ingresar los detalles del paquete: ");
+                Paquete_act = new CPaquete (Numero, Console.ReadLine());
+
+                Console.Write("Ingresar costo del paquete: $");
+                Paquete_act.SetCosto(float.Parse(Console.ReadLine()));
+
+                if (Menor == null || Paquete_act.EsMasBaratoQue(Menor) == true)
+                    Menor = Paquete_act;
+
+                Console.Write("\n\n\n\n"+ Paquete_act.DarDatos());
+
+                Console.Write("\n\n\nIngresar codigo del paquete: ");
+                Numero = ushort.Parse(Console.ReadLine());
+
+            }
+
+            if (Menor != null)
+            {
+                Console.Write("\n\n\n\nEl paquete mas barato es: \n\n" + Menor.DarDatos());
+                Console.Write("\n\n\n\nEl valor del paquete en 1 Cuota es : $" + Menor.DarPrecio());
+                Console.Write("\n\n\n\nEl valor del paquete en 12 Cuotas es : $" + Menor.DarPrecio(12));
+            }
+            else
+                Console.Write("\n\n\n\nNo se ingresaron paquetes turísticos válidos");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
---
+++ Agencia De Turismo/Agencia De Turismo/CPaquete.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agencia_De_Turismo
+{
+    class CPaquete
+    {
+        private static float Impuesto;
+
+        private uint Num_Paq;
+        private string Detalle;
+        private float Costo;
+
+
+
+        public static void SetImpuesto(float porcentaje)
+        {
+            CPaquete.Impuesto = porcentaje;
+        }
+
+
+        public CPaquete(uint numero, string descripcion)
+        {
+            this.Num_Paq = numero;
+            this.Detalle = descripcion;
+        }
+
+
+        public void SetCosto(float monto)
+        {
+            this.Costo = monto;
+        }
+
+
+        public float GetCosto()
+        {
+            return this.Costo;
+        }
+
+
+        public float DarPrecio()
+        {
+            return this.Costo * (1F + CPaquete.Impuesto / 100);
+        }
+
+        public float DarPrecio(ushort cuotas)
+        {
+            return this.DarPrecio() * (1 + ((cuotas-1) * 0.1F)); 
+        }
+
+
+        public string DarDatos()
+        {
+            string datos = "\n Numero de paquete: " + this.Num_Paq.ToString();
+            datos += "\tDetalle: " + this.Detalle;
+            datos += "\tCosto: $" + this.Costo.ToString();
+            datos += "\tImpuesto: " + CPaquete.Impuesto.ToString() + "%";
+            datos += "\tPrecio total: $" + this.DarPrecio().ToString();
+
+            return datos;
+        }
+
+
+        public float COSTO
+            {
+            get { return this.Costo; }
+            set { this.Costo = value; }
+            }
+
+        public bool EsMasBaratoQue(CPaquete otroPaquete)
+        {
+            if (this.DarPrecio() < otroPaquete.DarPrecio())
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
